fc9793b6442b87065231c0534f36d0ae20c79472
zhunliu <zhunliu@microsoft.com>
2020-07-31 18:45:30 +0000

    Update the experiment scripts

---
+++ scripts/node1/watch_tnlrv3_large_dense.sh
@@ -0,0 +1,32 @@
+BERT_DATA_PATH=/turing-nfs/users/zhunliu/datasets/bert_data_cnndm_final/cnndm
+MODEL_PATH=/turing-nfs/users/zhunliu/dump/bertsum_checkpoints/$2
+WEIGHTS_PATH=/turing-nfs/users/zhunliu/dump/checkpoints/tnlrv3/tnlrv3-large-uncased.pt
+
+mkdir -p $MODEL_PATH
+mkdir -p ../logs/$2
+
+    # -visible_gpus 0,1,2,3,4,5,6,7 \
+CUDA_VISIBLE_DEVICES=13 \
+PYTHONIOENCODING=utf8 \
+python \
+    train.py \
+    -task ext \
+    -mode validate \
+    -bert_data_path $BERT_DATA_PATH \
+    -model_type tnlrv3-large-uncased \
+    -sparse false \
+    -weights_path $WEIGHTS_PATH \
+    -ext_dropout 0.1 \
+    -model_path $MODEL_PATH \
+    -lr 2e-3 \
+    -visible_gpus 13 \
+    -report_every 50 \
+    -save_checkpoint_steps 1000 \
+    -batch_size 3000 \
+    -checkpoint_segment 0 \
+    -train_steps 50000 \
+    -accum_count 1 \
+    -log_file ../logs/$2/validate.log \
+    -use_interval true \
+    -warmup_steps 10000 \
+    -max_pos $1
---
+++ scripts/node2/train_bert_large.sh
@@ -0,0 +1,29 @@
+BERT_DATA_PATH=/turing-nfs/users/zhunliu/datasets/bert_data_cnndm_final/cnndm
+MODEL_PATH=/turing-nfs/users/zhunliu/dump/bertsum_checkpoints/$2
+
+mkdir -p $MODEL_PATH
+mkdir -p ../logs/$2
+
+    # -visible_gpus 0,1,2,3,4,5,6,7 \
+PYTHONIOENCODING=utf8 \
+python \
+    train.py \
+    -task ext \
+    -mode train \
+    -bert_data_path $BERT_DATA_PATH \
+    -model_type bert-large-uncased \
+    -sparse false \
+    -ext_dropout 0.1 \
+    -model_path $MODEL_PATH \
+    -lr 2e-3 \
+    -visible_gpus 4,5,6 \
+    -report_every 50 \
+    -save_checkpoint_steps 1000 \
+    -batch_size 1536 \
+    -checkpoint_segment 0 \
+    -train_steps 50000 \
+    -accum_count 4 \
+    -log_file ../logs/$2/run.log \
+    -use_interval true \
+    -warmup_steps 10000 \
+    -max_pos $1
